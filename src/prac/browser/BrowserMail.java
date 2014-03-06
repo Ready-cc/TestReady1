baf203759a17e8850fcdae9ab3a92ca36db7ac8b
ready-cc <erdinghao@126.com>
2014-03-06 11:49:18 +0800

    更新

--- src/prac/browser/BrowserMail.java
+++ src/prac/browser/BrowserMail.java
@@ -1,65 +1,66 @@
 package prac.browser;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class BrowserMail {
-	private WebDriver ie;
-	private static DesiredCapabilities caps = null;
-	private String projectpath = System.getProperty("user.dir");
 
+
+public class BrowserMail {
+	private WebDriver driver;
+	
 	@BeforeClass
 	public void start(){
-    	System.setProperty("webdriver.ie.driver", projectpath+"/tool/IEDriverServer32.exe"); 
-		caps = DesiredCapabilities.internetExplorer();
-		caps.setCapability(InternetExplorerDriver.FORCE_CREATE_PROCESS, false);
-		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);   
-		caps.setCapability(InternetExplorerDriver.IE_SWITCHES, "-private");		
-        caps.setCapability("ignoreZoomSetting", true);
-        ie = new InternetExplorerDriver(caps);
+		BrowserInit browser = new  BrowserInit(BrowserType.ie);
+		driver =  browser.driver;
 	}
 	@Test
 	public void logOn(){
-		ie.get("http://mail.126.com");
-		ie.findElement(By.xpath("//input[@id='idInput']")).sendKeys("erdinghao");
-		ie.findElement(By.xpath("//input[@id='pwdInput']")).sendKeys("er15ding");
-		ie.findElement(By.xpath("//button[@id='loginBtn']")).click();	
+		driver.get("http://mail.126.com");
+		driver.findElement(By.xpath("//input[@id='idInput']")).sendKeys("erdinghao");
+		driver.findElement(By.xpath("//input[@id='pwdInput']")).sendKeys("er15ding");
+		driver.findElement(By.xpath("//button[@id='loginBtn']")).click();	
 		System.out.println("登陆成功");
 	}
 
 	@Test
 	public void writeLetter(){
 
-		ie.findElement(By.xpath("//div[@id='dvNavTop']/descendant::span[contains(text(),'写 信')]")).click();
-		ie.findElement(By.xpath("//div[@id='divComposeto']/descendant::input[2]")).sendKeys("erdinghao@126.com");
-		ie.findElement(By.xpath("//div[@id='divComposeSubject']/descendant::input")).sendKeys("test-ready");
-		ie.findElement(By.xpath("//header[@id='divComposeToolbar']/descendant::span[1]")).click();
+		driver.findElement(By.xpath("//div[@id='dvNavTop']/descendant::span[contains(text(),'写 信')]")).click();
+		driver.findElement(By.xpath("//div[@id='divComposeto']/descendant::input[2]")).sendKeys("erdinghao@126.com");
+		driver.findElement(By.xpath("//div[@id='divComposeSubject']/descendant::input")).sendKeys("test-ready");
+		driver.findElement(By.xpath("//header[@id='divComposeToolbar']/descendant::span[1]")).click();
 		System.out.println("test写信1");	
 	}
 
 	@Test
 	public void inBox(){
-		ie.findElement(By.xpath("//div[@id='navtree']/descendant::span[@title='收件箱']")).click();
-		ie.findElement(By.xpath("//div[contains(@id,'_ContentDiv')]/descendant::div[contains(text(),'test')][1]")).click();
+		//收件箱有时会定位不到
+		driver.findElement(By.xpath("//div[@id='navtree']/descendant::span[@title='收件箱']")).click();
+/*  读信操作--提示找不到元素
+		WebElement cq= driver.findElement(By.xpath("//div[contains(text(),'ready') and @class = 'cq']"));
+		if (!cq.isDisplayed()){System.out.println("Element is not displayed!");}
+		((JavascriptExecutor)driver).executeScript("arguments[0].checked = true;", cq);
+		driver.findElement(By.xpath("//div[contains(text(),'ready') and @class = 'cq']")).click();
 		System.out.println("test收件箱2");
+		*/
 	}
 	
 	@Test
 	public void outBox(){
-		ie.findElement(By.xpath("//div[@id='navtree']/descendant::span[(text()='已发送')]")).click();
+		driver.findElement(By.xpath("//div[@id='navtree']/descendant::span[(text()='已发送')]")).click();
 		System.out.println("test已发送4");
 	}
-	
+		
 	@AfterClass
 	public void releaseie(){
-		ie.quit();
+		driver.quit();
 	}
-	
+
 
 }
 
